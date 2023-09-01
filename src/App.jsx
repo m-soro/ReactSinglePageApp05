87fcf6d7a6e98a67144fea5c33b7dd3e61693372
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-01 17:48:26 -0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App container">
       <Header />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      <h4>Footer</h4>
+      <p>msoro</p>
     </div>
   );
 }
